a9469d2e1e25407b7f282da053fb9545e57561ad
Martin Solas <mac@MacBook-Pro-de-mac.local>
2017-07-10 10:30:51 -0300

    Inclusión de comprobaciónes en el formulario de Solicitudes de MAterial multimedia

--- app_reservas/form/solicitud.py
+++ app_reservas/form/solicitud.py
@@ -1,23 +1,33 @@
 from django import forms
 from django.forms import inlineformset_factory
 
-from app_reservas.models import Solicitud, HorarioSolicitud, EstadoSolicitud
+from app_reservas.models import Solicitud, HorarioSolicitud, EstadoSolicitud, Comision
 
 
 class SolicitudForm(forms.ModelForm):
 
-    comision = forms.ChoiceField(
+    comision = forms.CharField(
+        required=False,
         label='Comisión y Materia:',
         widget=forms.Select(attrs={'id': 'mat_select', 'class': 'form-control', 'disabled': 'true'}),
     )
 
     class Meta:
         model = Solicitud
-        fields = ['docente', 'tipoSolicitud', 'comision' ]
+        fields = ['docente', 'tipoSolicitud', 'comision']
         widgets = {
             'docente': forms.Select(attrs={'id': 'docente_select', 'class': 'form-control'}),
             'tipoSolicitud': forms.Select(attrs={'id': 'tipo_select', 'class': 'form-control', 'disabled': 'true'}),
         }
+
+    def clean_comision(self):
+        comision = self.cleaned_data['comision']
+        tipo_solicitud = self.cleaned_data['tipoSolicitud']
+        if tipo_solicitud.nombre == 'Cursado':
+            if not comision:
+                raise forms.ValidationError("La comision no puede estar vacia")
+        comision_obj = Comision.objects.get(id=comision)
+        return comision_obj
 
 
 class HorarioSolicitudForm(forms.ModelForm):
@@ -42,8 +52,31 @@ class HorarioSolicitudForm(forms.ModelForm):
             'tipoRecursoAli': forms.SelectMultiple(attrs={'class': 'form-control', 'disabled': 'true'}),
         }
 
+    def clean_fin(self):
+        inicio = self.cleaned_data['inicio']
+        fin = self.cleaned_data['fin']
+        if not fin:
+            raise forms.ValidationError(
+                "La hora de inicio no puede ser nula"
+            )
+        if inicio > fin:
+            raise forms.ValidationError(
+                "La hora de fin de la solicitud no puede ser menor a la hora de inicio"
+            )
+        return fin
 
-SolicitudInlineFormset = inlineformset_factory(Solicitud, HorarioSolicitud, form=HorarioSolicitudForm, extra=2)
+    def clean_incio(self):
+        inicio = self.cleaned_data['inicio']
+        if not inicio:
+            raise forms.ValidationError(
+                "La hora de inicio no puede ser nula"
+            )
+        return inicio
+
+
+
+
+SolicitudInlineFormset = inlineformset_factory(Solicitud, HorarioSolicitud, form=HorarioSolicitudForm, extra=3)
 
 class FilterSolicitudForm(forms.Form):
     estado = forms.ChoiceField(
