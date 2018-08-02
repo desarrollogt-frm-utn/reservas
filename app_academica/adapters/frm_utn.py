14340a30f4d73c3e98871aef7154bfd86170630d
martin94mza <martin_94_mza@hotmail.com>
2018-08-01 21:32:01 -0300

    Add cache to frm service

--- app_academica/adapters/frm_utn.py
+++ app_academica/adapters/frm_utn.py
@@ -2,53 +2,79 @@ from django.conf import settings
 from suds.client import Client
 import json
 import random
+from django.core.cache import cache
 
 
-def get_horarios():
+def get_horarios(from_cache=True):
+    if from_cache:
+        get_horarios_cache = cache.get('get_horarios')
+        if get_horarios_cache:
+            return json.loads(get_horarios_cache)
+
     url = settings.WSDL_URL
     client = Client(url)
-
     json_response = client.service.seticGetHorarios()
+    cache.add('get_horarios', json_response)
     parse_json = json.loads(json_response)
 
     return parse_json
 
 
-def get_alumnos():
+def get_alumnos(from_cache=True):
+    if from_cache:
+        get_alumnos_cache = cache.get('get_alumnos')
+        if get_alumnos_cache:
+            return json.loads(get_alumnos_cache)
     url = settings.WSDL_URL
     client = Client(url)
 
     json_response = client.service.SeticGetAlumnos()
+    cache.add('get_alumnos', json_response)
     parse_json = json.loads(json_response)
 
     return parse_json
 
 
-def get_especialidades():
+def get_especialidades(from_cache=True):
+    if from_cache:
+        get_especialidades_cache = cache.get('get_especialidades')
+        if get_especialidades_cache:
+            return json.loads(get_especialidades_cache)
     url = settings.WSDL_URL
     client = Client(url)
 
     json_response = client.service.seticGetEspecialidades()
+    cache.add('get_especialidades', json_response)
     parse_json = json.loads(json_response)
 
     return parse_json
 
 
-def get_materias():
+def get_materias(from_cache=True):
+    if from_cache:
+        get_materias_cache = cache.get('get_materias')
+        if get_materias_cache:
+            return json.loads(get_materias_cache)
     url = settings.WSDL_URL
     client = Client(url)
 
     json_response = client.service.seticGetMaterias()
+    cache.add('get_materias', json_response)
     parse_json = json.loads(json_response)
 
     return parse_json
 
 
-def get_comisiones_docentes(anio):
+def get_comisiones_docentes(anio, from_cache=True):
+    if from_cache:
+        get_comisiones_docentes_cache = cache.get('get_especialidades_{0!s}'.format(anio))
+        if get_comisiones_docentes_cache:
+            return json.loads(get_comisiones_docentes_cache)
     url = settings.WSDL_URL
     client = Client(url)
 
     json_response = client.service.seticGetComisionesDocentes(anio)
+    cache.add('get_especialidades_{0!s}'.format(anio), json_response)
     parse_json = json.loads(json_response)
 
     return parse_json
@@ -64,13 +90,32 @@ def get_cursado(anio, legajo):
     return parse_json
 
 
-def get_cantidad_inscriptos(anio, especialidad, plan, materia, comision):
+def get_cantidad_inscriptos(anio, especialidad, plan, materia, comision, from_cache=True):
     if settings.TEST:
         cantidad_inscriptos = random.randint(10, 30)
     else:
-        url = settings.WSDL_URL
-        client = Client(url)
-        cantidad_inscriptos = client.service.seticGetCantidadInscriptos(anio, especialidad, plan, materia, comision)
+        if from_cache:
+            get_comisiones_docentes_cache = cache.get('get_cantidad_inscriptos_{0!s}_{1!s}_{2!s}_{3!s}_{4!s}'.format(
+                anio,
+                especialidad,
+                plan,
+                materia,
+                comision
+            ))
+            if get_comisiones_docentes_cache:
+                cantidad_inscriptos = get_comisiones_docentes_cache
+
+            if not cantidad_inscriptos:
+                url = settings.WSDL_URL
+                client = Client(url)
+                cantidad_inscriptos = client.service.seticGetCantidadInscriptos(anio, especialidad, plan, materia, comision)
+                cache.add('get_cantidad_inscriptos_{0!s}_{1!s}_{2!s}_{3!s}_{4!s}'.format(
+                    anio,
+                    especialidad,
+                    plan,
+                    materia,
+                    comision
+                ), cantidad_inscriptos)
 
     response = {
         'cantidad': cantidad_inscriptos
@@ -79,11 +124,29 @@ def get_cantidad_inscriptos(anio, especialidad, plan, materia, comision):
     return response
 
 
-def get_horarios_comision(anio, especialidad, plan, materia, comision):
+def get_horarios_comision(anio, especialidad, plan, materia, comision, from_cache=True):
+    if from_cache:
+        get_horarios_comision_cache = cache.get('get_horarios_comision_{0!s}_{1!s}_{2!s}_{3!s}_{4!s}'.format(
+            anio,
+            especialidad,
+            plan,
+            materia,
+            comision
+        ))
+        if get_horarios_comision_cache:
+            return json.loads(get_horarios_comision_cache)
+
     url = settings.WSDL_URL
     client = Client(url)
 
     json_response = client.service.seticGetHorariosPorComision(anio, especialidad, plan, materia, comision)
+    cache.add('get_horarios_comision_{0!s}_{1!s}_{2!s}_{3!s}_{4!s}'.format(
+        anio,
+        especialidad,
+        plan,
+        materia,
+        comision
+    ), json_response)
     parse_json = json.loads(json_response)
 
     return parse_json
