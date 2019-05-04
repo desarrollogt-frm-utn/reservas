7196b017877c0afad5b6c68dc748d6160ce0fcde
Federico Gallardo <fedegallar2006@hotmail.com>
2019-05-04 10:41:24 -0300

    Added exceptions handling for Views

--- app_reservas/services/reservas.py
+++ app_reservas/services/reservas.py
@@ -27,25 +27,29 @@ def get_nombre_evento(docente_obj, comision_obj, nombre_evento = None):
 
 def crear_evento(reserva_obj):
     for horario_obj in reserva_obj.horarioreserva_set.filter(id_evento_calendar__isnull=True):
-        inicio = obtener_siguiente_dia_vigente(int(horario_obj.dia), horario_obj.inicio, reserva_obj.fecha_inicio)
-        fin = obtener_siguiente_dia_vigente(int(horario_obj.dia), horario_obj.fin, reserva_obj.fecha_inicio)
-        hasta = None
+        try:
+            inicio = obtener_siguiente_dia_vigente(int(horario_obj.dia), horario_obj.inicio, reserva_obj.fecha_inicio)
+            fin = obtener_siguiente_dia_vigente(int(horario_obj.dia), horario_obj.fin, reserva_obj.fecha_inicio)
+            hasta = None
 
-        if reserva_obj.comision and reserva_obj.fecha_fin:
-            hasta = obtener_fecha_finalizacion_reserva_cursado(reserva_obj.comision.semestre)
-        elif not reserva_obj.comision and reserva_obj.fecha_fin:
-            hasta = obtener_fecha_finalizacion_reserva_fuera_cursado(reserva_obj.fecha_fin)
-        
-        recurso_obj = get_recurso_obj(reserva_obj.recurso.id)
-        if recurso_obj:
-            crear_evento_recurso_especifico(
-                calendar_id=recurso_obj.calendar_codigo,
-                titulo=reserva_obj.nombre_evento,
-                inicio=inicio,
-                fin=fin,
-                hasta=hasta,
-                reserva_horario_obj=horario_obj,
-            )
+            if reserva_obj.comision and reserva_obj.fecha_fin:
+                hasta = obtener_fecha_finalizacion_reserva_cursado(reserva_obj.comision.semestre)
+            elif not reserva_obj.comision and reserva_obj.fecha_fin:
+                hasta = obtener_fecha_finalizacion_reserva_fuera_cursado(reserva_obj.fecha_fin)
+            
+            recurso_obj = get_recurso_obj(reserva_obj.recurso.id)
+            if recurso_obj:
+                evento = crear_evento_recurso_especifico(
+                        calendar_id=recurso_obj.calendar_codigo,
+                        titulo=reserva_obj.nombre_evento,
+                        inicio=inicio,
+                        fin=fin,
+                        hasta=hasta,
+                        reserva_horario_obj=horario_obj,
+                    )
+                return True
+        except Exception:
+            return False
 
 """
 ESTADOS RESERVA
