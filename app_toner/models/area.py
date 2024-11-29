6e25f2cc0257b6216629fc105d6a96916875f8c3
Augusto Riquelme <146036948+OTSUGUA14@users.noreply.github.com>
2024-11-29 11:56:28 -0300

    Delete app_toner directory

--- app_toner/models/area.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.db import models
-from django.template.defaultfilters import slugify
-
-class Area(models.Model):
-    #atributos
-    nombre = models.TextField()
-    
--- app_toner/models/cuerpo.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.db import models
-from django.template.defaultfilters import slugify
-
-class Cuerpo(models.Model):
-    #atributos
-    nombre = models.TextField()
-    
